379f52346afbdf1eee4e2ac2df39038f5c8ae006
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-03-20 00:00:32 -0500

    chore(config): normalizes monorepo

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,5 +1,5 @@
 /* eslint import/no-default-export: 0 */
 /* eslint import/no-cycle: 0 */
-import { defineConfig, vitestRemix } from '@kurocado-studio/qa';
+import { defineConfig, vitestReact } from '@kurocado-studio/qa';
 
-export default defineConfig(vitestRemix);
+export default defineConfig(vitestReact);
