9ab5d21fcfcd210ed503aa056f6e16a404d1f7da
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-03-30 18:09:23 -0500

    feat(ui): adds success variant

--- packages/buttons/src/Button.tsx
+++ packages/buttons/src/Button.tsx
@@ -6,7 +6,7 @@ import { tv } from 'tailwind-variants';
 
 export type ButtonProps = React.PropsWithChildren<
   AriaButtonOptions<'button'> & {
-    variant?: 'primary' | 'secondary' | 'destructive' | 'icon';
+    variant?: 'primary' | 'secondary' | 'destructive' | 'success' | 'icon';
     className?: string;
   }
 >;
@@ -20,6 +20,8 @@ const buttonStyles = tv({
       secondary:
         'bg-gray-100 hover:bg-gray-200 pressed:bg-gray-300 text-gray-800 dark:bg-zinc-600 dark:hover:bg-zinc-500 dark:pressed:bg-zinc-400 dark:text-zinc-100',
       destructive: 'bg-red-700 hover:bg-red-800 pressed:bg-red-900 text-white',
+      success:
+        'bg-green-700 hover:bg-green-800 pressed:bg-green-900 text-white',
       icon: 'border-0 p-1 flex items-center justify-center text-gray-600 hover:bg-black/[5%] pressed:bg-black/10 dark:text-zinc-400 dark:hover:bg-white/10 dark:pressed:bg-white/20 disabled:bg-transparent',
     },
     isDisabled: {
