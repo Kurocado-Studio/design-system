ee275675be650ebe518ef01d3327ac55556c54b1
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-04-17 09:23:03 -0500

    chore(config): reorganizes files

---
+++ packages/ui/src/domain/primitives/index.ts
@@ -0,0 +1,26 @@
+import { getTailwindBorderRadius } from './borderRadius';
+import { getTailwindColors } from './colors';
+import { getTailwindDimensions } from './dimensions';
+import { getTailwindFontSize } from './typography';
+
+export function composePrimitives(payload: Record<string, unknown>): {
+  primitives: Record<string, Record<string, unknown>>;
+  primitivesCssVariables: Record<string, unknown>;
+} {
+  const { fontSize, fontSizeCssVariableMap } = getTailwindFontSize(payload);
+  const { colors, colorCssVariableMap } = getTailwindColors(payload);
+  const { dimensions, dimensionsCssVariableMap } =
+    getTailwindDimensions(payload);
+  const { borderRadius, borderRadiusCssVariableMap } =
+    getTailwindBorderRadius(payload);
+
+  return {
+    primitives: { spacing: dimensions, fontSize, colors, borderRadius },
+    primitivesCssVariables: {
+      ...colorCssVariableMap,
+      ...dimensionsCssVariableMap,
+      ...fontSizeCssVariableMap,
+      ...borderRadiusCssVariableMap,
+    },
+  };
+}
