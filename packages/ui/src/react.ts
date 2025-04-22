58ad817aa2785f92ac29d96f53df66fd508b216f
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-04-22 09:29:46 -0500

    chore(config): updates README and exports types (#90)
    * chore(docs): updates README
    
    * fix(config): exports types

--- packages/ui/src/react.ts
+++ packages/ui/src/react.ts
@@ -1,3 +1,7 @@
 export type * from 'src/domain/types';
+
 export * from 'src/components/cards/Card';
+export type * from 'src/components/cards/Card';
+
 export * from 'src/context/ThemeProvider';
+export type * from 'src/context/ThemeProvider';
