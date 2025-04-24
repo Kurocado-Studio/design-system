fe839833b469e811bd166b5e7e326d6c7a2aeb57
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-04-24 18:43:20 -0500

    chore(config): changes to vite config

---
+++ packages/ui/src/vue/components/cards/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="mergedClass" v-bind="$attrs" />
+</template>
+
+<script lang="ts" setup>
+import { twMerge } from 'tailwind-merge';
+import { computed, toRefs } from 'vue';
+
+import { type CardProps } from 'src/lib';
+import { cardComponentWrapperClasses } from 'src/lib';
+
+const props = defineProps<CardProps>();
+
+const { className } = toRefs(props);
+
+const mergedClass = computed(() => {
+  return twMerge(cardComponentWrapperClasses(), className.value);
+});
+</script>
