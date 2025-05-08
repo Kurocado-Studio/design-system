cad5e7bfe7462290d3842e5b647a2249db239ded
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-05-07 20:00:47 -0500

    fix(config): fixes paths on exports

--- packages/ui/src/react/exports.ts
+++ packages/ui/src/react/exports.ts
@@ -1,6 +1,6 @@
-export type * from 'src/lib/domain/types';
-export * from 'src/react/components/Card/Card';
-export * from 'src/react/components/Grid/Grid';
-export * from 'src/react/components/Input/Input';
-export * from 'src/react/components/Typography/Typography';
-export * from 'src/react/context/ThemeProvider';
+export type * from '../lib/domain/types';
+export * from './components/Card/Card';
+export * from './components/Grid/Grid';
+export * from './components/Input/Input';
+export * from './components/Typography/Typography';
+export * from './context/ThemeProvider';
