5ce6f0b5eb0f6f259da65e3d14b55aa38dfb5ff4
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-05-08 04:13:49 -0500

    fix(config): fixes tsup so it doesnt fail on vercel

--- packages/ui/src/vue/exports.ts
+++ packages/ui/src/vue/exports.ts
@@ -1,8 +1,8 @@
 import ThemeProvider from './context/ThemeProvider.vue';
 
-export { Card, CardHeader, CardBody, CardFooter } from './components/card';
+export { Card, CardHeader, CardBody, CardFooter } from './components/Card';
 export type { CardComponentProps } from '../lib';
 
-export { Input } from './components/input';
+export { Input } from './components/Input';
 
 export { ThemeProvider };
