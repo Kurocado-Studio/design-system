d3a5ffb707bd488679d7ca76b06bf6355a2520bd
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-05-07 20:00:47 -0500

    fix(config): fixes paths on exports

--- packages/ui/src/vue/exports.ts
+++ packages/ui/src/vue/exports.ts
@@ -1,7 +1,7 @@
 import ThemeProvider from './context/ThemeProvider.vue';
 
 export { Card, CardHeader, CardBody, CardFooter } from './components/card';
-export type { CardComponentProps } from 'src/lib';
+export type { CardComponentProps } from '../lib';
 
 export { Input } from './components/input';
 
