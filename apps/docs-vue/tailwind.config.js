a6f0beede4cacf468e031d8c41331903e90c61d2
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-05-12 04:36:01 -0500

    refactor(config): reformats with prettier

--- apps/docs-vue/tailwind.config.js
+++ apps/docs-vue/tailwind.config.js
@@ -3,6 +3,6 @@ import preset from '@kurocado-studio/ui/tailwind.preset';
 export const content = [
   './src/**/*.{ts,tsx}',
   './stories/**/*.{ts,tsx}',
-  './node_modules/@kurocado-studio/ui/dist/**/*.js',
+  './node_modules/@kurocado-studio/ui/**/*.js',
 ];
 export const presets = [preset];
