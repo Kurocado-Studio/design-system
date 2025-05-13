d1d77964c20f29fef30507ee66fb2f0ee08e2005
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-05-13 10:54:33 -0500

    refactor(config): adds Avatar tokens

--- packages/ui/src/lib/domain/components/Avatar/tokens.ts
+++ packages/ui/src/lib/domain/components/Avatar/tokens.ts
@@ -1,9 +1,9 @@
 import { type AvatarComponentToken } from './types';
 
 export const tailwindAvatarComponentToken: AvatarComponentToken = {
-  bg: 'bg-card-root-bg-default dark:bg-gray-900',
-  borderRadius: 'rounded-card-root-radius-default',
-  fg: 'text-card-root-fg-default dark:text-gray-100',
+  bg: 'bg-avatar-root-bg-default dark:bg-gray-900',
+  borderRadius: 'rounded-avatar-root-radius-default',
+  fg: 'text-avatar-root-fg-default dark:text-gray-100',
   border: 'border-2 border-gray-200 dark:border-gray-800',
 };
 
